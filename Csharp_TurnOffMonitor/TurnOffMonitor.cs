d8c6ccc61b863eeaa770bc7b297b903da794b888
王義凱 <a73013110@gmail.com>
2019-07-26 16:27:12 +0800

    加入專案檔案。

---
+++ Csharp_TurnOffMonitor/TurnOffMonitor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Csharp_HUD;
+
+namespace Csharp_TurnOffMonitor
+{
+    class TurnOffMonitor
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Console.ReadLine();
+        }
+    }
+}
